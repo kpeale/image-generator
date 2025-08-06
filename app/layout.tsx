0eb0b5f874ef6324fd01c6f7342494cbd4e17589
Kpeale Legbara <66691866+kpeale@users.noreply.github.com>
2025-08-07 00:01:08 +0100

    i removed the toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from 'next/font/google';
 import './globals.css';
 import ResponsiveNav from '@/components/Home/Navbar/ResponsiveNav';
-import { Toaster } from '@/components/ui/sonner';
+
 
 const font = Roboto({
   weight: ['100', '300', '400', '500', '700', '900'],
@@ -24,7 +24,6 @@ export default function RootLayout({
       <body className={`${font.className} bg-gray-900  antialiased`}>
         <ResponsiveNav />
         {children}
-        <Toaster />
       </body>
     </html>
   );
